9cac4a7d053b88d36f475305b93569188781532a
Lomiredos <baudoineliott1@gmail.com>
2026-05-17 16:49:17 +0200

    init

---
+++ include/Renderer3D.hpp
@@ -0,0 +1,10 @@
+// eliott-renderer-3d — Renderer3D.hpp
+// Module de rendu 3D basé sur Raylib
+// À implémenter
+
+#pragma once
+
+namespace ee::renderer
+{
+
+} // namespace ee::renderer
---
+++ src/Renderer3D.cpp
@@ -0,0 +1,10 @@
+// eliott-renderer-3d — Renderer3D.cpp
+// Module de rendu 3D basé sur Raylib
+// À implémenter
+
+#include "Renderer3D.hpp"
+
+namespace ee::renderer
+{
+
+} // namespace ee::renderer
